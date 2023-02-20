1eb1aee953c043b776c55724704bcede62534b55
beresdev <beresdev@gmail.com>
2023-02-20 13:33:18 -0600

    router implementation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,12 @@
-export function Login () {
-    return (
-        <h1>Login</h1>
-    )
+import { Link } from "react-router-dom";
+
+export function Login() {
+  return (
+    <>
+      <h1>Login</h1>
+      <button>
+        <Link to= "/MenuW">Iniciar Sesión</Link>
+      </button>
+    </>
+  );
 }
--- src/components/MyOrders.jsx
+++ src/components/MyOrders.jsx
@@ -1,5 +1,12 @@
-export function MyOrders () {
-    return (
-        <h1>MyOrders</h1>
-    )
+import { Link } from "react-router-dom";
+
+export function MyOrders() {
+  return (
+    <>
+      <h1>MyOrders</h1>
+      <button>
+        <Link to="/MenuW">Menu</Link>
+      </button>
+    </>
+  );
 }
