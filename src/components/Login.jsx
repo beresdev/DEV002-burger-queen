5c7f844c7914b898eb4804609deb3b1020642797
beresdev <beresdev@gmail.com>
2023-02-20 19:16:08 -0600

    logins styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,10 +3,20 @@ import { Link } from "react-router-dom";
 export function Login() {
   return (
     <>
-      <h1>Login</h1>
-      <button>
-        <Link to= "/MenuW">Iniciar Sesión</Link>
-      </button>
+    <section className="login-container">
+      <div className="logo-container">
+        <img src="/LasChidas_500x500.png" alt="logotipo-las-chidas" />
+      </div>
+      <form className="form">
+        <label>User name: </label>
+        <input type="text" />
+        <label>Pasword: </label>
+        <input type="text" />
+        <button>
+          <Link to="/MenuW">Iniciar Sesión</Link>
+        </button>
+      </form>
+    </section>
     </>
   );
 }
