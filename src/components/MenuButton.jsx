f88b3555d09ec112e55d3e81463e4f3d52a2a360
beresdev <beresdev@gmail.com>
2023-02-24 11:47:35 -0600

    finished styles for NewOrder page

---
+++ src/components/MenuButton.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export function MenuButton(props) {
+  return(
+    <Link to={props.route}>
+      <button className="menu-button">{props.text}</button>
+    </Link>
+  )
+}
